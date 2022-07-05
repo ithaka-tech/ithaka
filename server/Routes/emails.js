bace936612bde9149fd310910be3936038d1e522
Kazu2002 <69529612+Kazu2002@users.noreply.github.com>
2022-07-05 18:13:50 -0400

    Delete server directory

--- server/Routes/emails.js
+++
@@ -1,5 +0,0 @@
-//this is the module used to keep track of valid email links to customers
-//it is also used to send the email to the specified customer based on an email parameter
-
-const express = require('express');
-const nodemailer = require('nodemailer');
